e5c0c347144a4b95805a98f942de5bb92213bfd0
TradingXZJL <zhoujunlin@tradingx.io>
2024-08-23 18:22:13 +0800

    add query spot margin api

--- private_rest_res_margin.go
+++ private_rest_res_margin.go
@@ -7,3 +7,12 @@ type SpotMarginTradeSetLeverageRes struct {
 	RetExtInfo struct{} `json:"retExtInfo"`
 	Time       int64    `json:"time"`
 }
+
+// spotLeverage	string	槓桿倍數. 如果處於關閉狀態的話, 則返回 ""
+// spotMarginMode	string	開關狀態. 1: 開啟, 0: 關閉
+// effectiveLeverage	string	實際借貸槓桿倍數。 精度保留2位小數，向下截取
+type SpotMarginTradeStateRes struct {
+	SpotLeverage      string `json:"spotLeverage"`      // 槓桿倍數. 如果處於關閉狀態的話, 則返回 ""
+	SpotMarginMode    string `json:"spotMarginMode"`    // 開關狀態. 1: 開啟, 0: 關閉
+	EffectiveLeverage string `json:"effectiveLeverage"` // 實際借貸槓桿倍數。 精度保留2位小數，向下截取
+}
